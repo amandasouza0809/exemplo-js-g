6eee52eec8ec38af886c73145b5f5a54d052a83b
amandasouza0809 <rm97626@fiap.com.br>
2023-03-07 11:49:34 -0300

    script.js adicionado ao html

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//Este é um comentario de linha 
+
+/**
+ * É um comentario
+ * de 
+ * Bloco
+ */
+
+console.log ("Olá Mundo")
+console.error ("Bom Dia!")
+console.warn ("Será?")
+console.info ("Vai ser...")
